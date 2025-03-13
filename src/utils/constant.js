44b8a0cf326e4fe9e8024c3d09b9246dc7837958
Ayush Kumar <ayushagrawal.5288@gmail.com>
2025-03-13 22:29:39 +0530

    Ignore constant.js file

--- src/utils/constant.js
+++
@@ -1,8 +0,0 @@
-const API= "AIzaSyAVs6n-tJrglbWHkI8jLi6wP4gFZSsFamQ";
-
-export const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=36&regionCode=IN&key="+API;
-
-
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-
-export const limit=30;
